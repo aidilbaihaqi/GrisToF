8ca2fa7c006e7da34eae7828001254ee0e93a9e3
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-02-28 22:01:14 +0700

    model and migration: users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username',100)->nullable()->unique();
             $table->string('password');
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
