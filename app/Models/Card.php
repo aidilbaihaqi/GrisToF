0fd1bd8b2438c76596a0c7b8c5e5c961471a2c8f
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-03-01 00:36:30 +0700

    model and migration: Card

---
+++ app/Models/Card.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\CardPriority;
+use App\Enums\CardStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'workspace_id',
+        'title',
+        'description',
+        'deadline',
+        'status',
+        'order',
+        'status',
+        'priority'
+    ];
+
+    protected function casts(): array 
+    {
+        return [
+            'status' => CardStatus::class,
+            'priority' => CardPriority::class
+        ];
+    }
+}
