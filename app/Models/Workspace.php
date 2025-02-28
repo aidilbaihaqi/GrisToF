26ee9bb22b141c4a914ab9836f7bea5e2977b5b8
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-02-28 22:33:08 +0700

    model and migration: workspace

---
+++ app/Models/Workspace.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\WorkspaceVisibility;
+use Illuminate\Database\Eloquent\Model;
+
+class Workspace extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name', 
+        'cover',
+        'logo',
+        'visibility'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'visibility' => WorkspaceVisibility::class
+        ];
+    }
+}
