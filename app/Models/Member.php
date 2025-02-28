7ed6026592ef68b2814ee959144f4096ce6bf465
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-03-01 00:55:20 +0700

    relational table

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Member extends Model
 {
     protected $guarded = [];
+    protected $with = ['user'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function memorable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
